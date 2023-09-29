ac2429f4d5199d4ac77ed725d31b67e7ca0066d1
Nicolas Fuentes <nfuentes@frba.utn.edu.ar>
2023-09-29 04:31:01 -0300

    version inicial pantalla de login, falta integrar con API

---
+++ app/src/main/java/ar/com/utn/devmobile/servimatch/LoginActivity.kt
@@ -0,0 +1,145 @@
+package ar.com.utn.devmobile.servimatch
+
+import android.os.Bundle
+import android.os.Message
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
+import ar.com.utn.devmobile.servimatch.ui.theme.MyApplicationTheme
+import ar.com.utn.devmobile.servimatch.ui.theme.Turquesa3
+import ar.com.utn.devmobile.servimatch.ui.theme.Turquesa1
+import ar.com.utn.devmobile.servimatch.ui.theme.Turquesa2
+import ar.com.utn.devmobile.servimatch.ui.theme.Turquesa5
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                LoginScreen()
+            }
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen() {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isButtonEnabled by remember { mutableStateOf(false) }
+    var paddingH = 16.dp
+    var paddingV = 8.dp
+    val painterIcon = painterResource(id = R.drawable.servimatch)
+    var isError by remember { mutableStateOf(false) }
+    var errorMessage by remember { mutableStateOf("") }
+    var errorIcon by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Turquesa1),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center)
+    {
+        Box(modifier = Modifier.padding(horizontal = 0.dp, vertical = 32.dp)) {
+            Image(painter = painterIcon, contentDescription = null)
+        }
+        OutlinedTextField(
+            value = username,
+            onValueChange = {
+                username = it
+                isButtonEnabled = it.isNotBlank() && password.isNotBlank()
+            },
+            label = { Text("Usuario") },
+            modifier = Modifier
+                .padding(horizontal = paddingH, vertical = paddingV)
+                .fillMaxWidth(),
+            singleLine = true,
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Turquesa3, // Color del borde cuando el campo está enfocado
+                unfocusedBorderColor = Turquesa5, // Color del borde cuando el campo no está enfocado
+                focusedLabelColor = Turquesa3,
+                textColor = Turquesa5
+            )
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+                isButtonEnabled = it.isNotBlank() && username.isNotBlank()
+            },
+            isError = isError,
+            label = { Text("Contraseña") },
+            modifier = Modifier
+                .padding(horizontal = paddingH, vertical = paddingV)
+                .fillMaxWidth(),
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            supportingText={ Text(errorMessage) },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Turquesa3, // Color del borde cuando el campo está enfocado
+                unfocusedBorderColor = Turquesa5, // Color del borde cuando el campo no está enfocado
+                focusedLabelColor = Turquesa3,
+                textColor = Turquesa5,
+                errorSupportingTextColor = Color.Red
+            )
+        )
+        //Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                // Logica de Login
+                if (username == "usuario" && password == "contrasena") {
+                    isError=false
+                    errorMessage="Usuario Logueado"
+                } else {
+                    isError=true
+                    errorMessage="Usuario y/o Contraseña incorrectos"
+                }
+            },
+            enabled = isButtonEnabled,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Turquesa3,
+                contentColor = Color.White,
+                disabledContainerColor = Turquesa2,
+                disabledContentColor = Turquesa3
+            ),
+            modifier = Modifier
+                .padding(horizontal = paddingH, vertical = paddingV)
+                .fillMaxWidth()
+        ) {
+            Text(text = "Iniciar sesión")
+        }
+        Text(
+            text = "Olvidaste la contraseña?",
+            style = MaterialTheme.typography.bodySmall,
+            color = Turquesa5,
+            modifier = Modifier
+                .padding(horizontal = paddingH, vertical = paddingV)
+        )
+    }
+
+}
+
--- app/src/main/java/ar/com/utn/devmobile/servimatch/ui/theme/Color.kt
+++ app/src/main/java/ar/com/utn/devmobile/servimatch/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Turquesa1 = Color(0xFFD1F3EB)
+val Turquesa2 = Color(0xFFA3E7D6)
+val Turquesa3 = Color(0xFF48CFAD)
+val Turquesa4 = Color(0xFF246857)
+val Turquesa5 = Color(0xFF12342B)
+
+val Purpura1 = Color(0xFFB3A8D2)
+val Purpura2 = Color(0xFF6750A4)
+val Purpura3 = Color(0xFF342852)
