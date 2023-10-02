c588d8fb98870494fb204406e47b4469cae3e9a7
unknown <agussaturni@frba.utn.edu.ar>
2023-10-02 00:40:07 -0300

    añadi 3 filtros hardcodeados y un texto de recomendados hardcodeado

--- app/src/main/java/ar/com/utn/devmobile/servimatch/ui/main/HomeActivity.kt
+++ app/src/main/java/ar/com/utn/devmobile/servimatch/ui/main/HomeActivity.kt
@@ -26,14 +26,19 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import ar.com.utn.devmobile.servimatch.R
+import ar.com.utn.devmobile.servimatch.ui.theme.PurpleGrey40
 import ar.com.utn.devmobile.servimatch.ui.theme.Turquesa1
 import ar.com.utn.devmobile.servimatch.ui.theme.TurquesaTituloHome
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen( navController: NavController,username: String) {
-    var paddingH = 12.dp
+    var paddingH = 15.dp
     var paddingV = 15.dp
+    val zones = listOf("Almagro", "Chacarita", "Flores")
+    val jobs = listOf("Plomero", "Cerrajero", "Electricista")
+    val prices = listOf("De Mayor a Menor", "De Menor a Mayor")
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -51,25 +56,38 @@ fun HomeScreen( navController: NavController,username: String) {
                 modifier = Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Filters(modifier = Modifier.weight(0.7f)) // Primer conjunto de filtros
-                Spacer(modifier = Modifier.width(10.dp)) // Espacio entre los filtros
-                Filters(modifier = Modifier.weight(0.7f)) // Segundo conjunto de filtros
-                Spacer(modifier = Modifier.width(10.dp)) // Espacio entre los filtros
-                Filters(modifier = Modifier.weight(0.7f)) // Tercer conjunto de filtros
-
+                 Filters(modifier = Modifier.weight(0.7f),"zona",zones) // Primer conjunto de filtros
+                 Spacer(modifier = Modifier.width(10.dp)) // Espacio entre los filtros
+                 Filters(modifier = Modifier.weight(0.7f),"rubro",jobs) // Primer conjunto de filtros
+                 Spacer(modifier = Modifier.width(10.dp)) // Espacio entre los filtros
+                 Filters(modifier = Modifier.weight(0.7f),"precio",prices) // Primer conjunto de filtros
+                 Spacer(modifier = Modifier.width(10.dp)) // Espacio entre los filtros
             }
-
+            Spacer(modifier = Modifier.height(25.dp)) // Espacio entre los filtros
+            ProvidersList()
 
         }
     }
 }
 
 @Composable
-fun Filters(modifier: Modifier = Modifier) {
-    var expanded by remember { mutableStateOf(false) }
-    var selectedZone by remember { mutableStateOf("Zona") }
+fun ProvidersList() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(text = "Recomendados (3)",
+            style = MaterialTheme.typography.titleLarge,
+            color = PurpleGrey40,)
+    }
 
-    val zones = listOf("Barrio 1", "Barrio 2", "Barrio 3")
+}
+
+@Composable
+fun Filters(modifier: Modifier = Modifier, categoria:String,items:List<String>) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedItem by remember { mutableStateOf(categoria) }
 
     Card(
         modifier = modifier
@@ -91,7 +109,7 @@ fun Filters(modifier: Modifier = Modifier) {
                         .fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Text(selectedZone, fontWeight = FontWeight.Bold)
+                    Text(selectedItem, fontWeight = FontWeight.Bold)
                     Icon(
                         imageVector = Icons.Default.ArrowDropDown,
                         contentDescription = null,
@@ -106,13 +124,13 @@ fun Filters(modifier: Modifier = Modifier) {
         onDismissRequest = { expanded = false },
         modifier = Modifier.fillMaxWidth()
     ) {
-        zones.forEach { zone ->
+        items.forEach { item ->
             DropdownMenuItem(
                 onClick = {
-                    selectedZone = zone
+                    selectedItem = item
                     expanded = false
                 },
-                text = { Text(zone, color = Color.Black) }
+                text = { Text(item, color = Color.Black) }
             )
         }
     }
