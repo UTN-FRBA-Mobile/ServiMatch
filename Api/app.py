b2774cef256a2349846201d3900b2364e008864e
Fernando Rabinovich <frabinovich@frba.utn.edu.ar>
2023-10-26 22:25:14 -0300

    Nuevos endpoints y login

--- Api/app.py
+++ Api/app.py
@@ -1,5 +1,5 @@
 import sys
-from flask import Flask
+from flask import Flask, request
 import datetime
 from flask import Flask
 from flask import Blueprint, jsonify
@@ -16,16 +16,131 @@ def healthcheck():
 @app.route('/tipoProfesion', methods=['GET'])
 def tipoServicio():
     try:
-        profesiones = [
-            'plomeria',
-            'gasista',
-            'techista',
-            'envios'
-        ]
+        profesiones = [	
+				'Electricista'
+				,'Plomero'
+				,'Carpintero'
+				,'Albañil'
+				,'Mecánico'
+				,'Jardinero'
+				,'Herrero'
+				,'Zapatero'
+				,'Costurero'
+				,'Pintor'
+				,'Gasista'
+				,'Limpieza a domicilio'
+				,'Técnico en aire acondicionado'
+				,'Técnico en refrigeración'
+				,'Técnico en computadoras'
+				,'Técnico en electrodomésticos'
+		]	
 
         return jsonify(profesiones), 200
     except Exception as e:
         return {'error': str(e)}, 500
+
+@app.route('/valoraciones', methods=['GET']) 
+def valoraciones():
+    try:
+        valoracion = [
+            	'0', '1', '1.5', '2', '2.5', '3', '3.5', '4', '4.5', '5',
+            	'5.5', '6', '6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10'
+        	]
+
+        return jsonify(valoracion), 200
+    except Exception as e:
+        return {'error': str(e)}, 500
+
+@app.route('/login',methods=['POST'])
+def login():
+    try:
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+
+        if username == "serviceMatchUser" and password == "serviceMatch1234":
+            return jsonify({'message': 'Login successful'}), 200
+        else:
+            return jsonify({'message': 'Login error'}), 403
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/zonas', methods=['GET'])
+def zonas():
+    try:
+        zonas = [	
+				'Recoleta'
+				,'Palermo'
+				,'San Telmo'
+				,'La Boca  '
+				,'Microcentro'
+				,'Belgrano'
+				,'Caballito'
+				,'Boedo'
+				,'Almagro'
+				,'Villa Crespo'
+				,'Villa Devoto'
+				,'Villa del Parque'
+				,'Núñez'
+				,'Saavedra'
+				,'Flores'
+				,'Villa Lugano'
+				,'Mataderos'
+				,'Parque Chacabuco'
+				,'Liniers'
+				,'Villa Urquiza'
+				,'Coghlan'
+				,'Villa Pueyrredón'
+				,'Balvanera'
+				,'Barracas'
+				,'Villa Riachuelo'
+				,'Parque Patricios'
+				,'Versalles'
+				,'Villa Luro'
+				,'Villa Santa Rita'
+				,'Villa Ortúzar'
+				,'Parque Avellaneda'
+				,'Villa General Mitre'
+				,'Villa Soldati'
+				,'Monte Castro'
+				,'Villa Real	'
+				,'Vélez Sársfield'
+				,'Villa Riachuelo'
+				,'Córdoba'
+				,'Rosario'
+				,'Mendoza'
+				,'Tucumán'
+				,'La Plata'
+				,'Mar del Plata'
+				,'Salta'
+				,'Santa Fe'
+				,'San Juan'
+				,'San Miguel de Tucumán'
+				,'San Luis'
+				,'Bariloche'
+				,'Bahía Blanca'
+				,'Neuquén'
+				,'Posadas'
+				,'Corrientes'
+				,'Resistencia'
+				,'San Salvador de Jujuy'
+				,'Formosa'
+				,'Santa Rosa'
+				,'Paraná'
+				,'Ushuaia'
+				,'Comodoro Rivadavia'
+				,'La Rioja'
+				,'Rawson'
+				,'Catamarca'
+				,'Santiago del Estero'
+				,'San Fernando del Valle de Catamarca'
+				,'Río Gallegos'
+				,'Viedma'
+		]	
+        return jsonify(zonas),200
+    except Exception as e:
+        return 500
+
 
 
 @app.route('/usuarios', methods=['GET'])
@@ -36,14 +151,17 @@ def comentarios():
 					'name': 'Fernando',
 					'lastname': 'Rabinovich',
 					'profesion': 'profesion',
+                    'puntajePromedio': 800,
 					'comentarios': {
 						'comentario1': {
 							'comentarioId': 1,
-							'mensaje': 'Mensaje 1'
+							'mensaje': 'Mensaje 1',
+                            'puntajeAsignado': 100
 						},
 						'comentario2': {
 							'comentarioId': 2,
-							'mensaje': 'Mensaje 2'
+							'mensaje': 'Mensaje 2',
+                            'puntajeAsignado': 100
 						}
 					}
 				},
@@ -51,14 +169,17 @@ def comentarios():
 					'name': 'Pedro',
 					'lastname': 'Bruno',
 					'profesion': 'profesion',
+                    'puntajePromedio': 800,
 					'comentarios': {
 						'comentario1': {
 							'comentarioId': 1,
-							'mensaje': 'Mensaje 1'
+							'mensaje': 'Mensaje 1',
+                            'puntajeAsignado': 100
 						},
 						'comentario2': {
 							'comentarioId': 2,
-							'mensaje': 'Mensaje 2'
+							'mensaje': 'Mensaje 2',
+                            'puntajeAsignado': 100
 						}
 					}
 				},
@@ -66,14 +187,17 @@ def comentarios():
 					'name': 'Hugo',
 					'lastname': 'Peykovick',
 					'profesion': 'profesion',
+                    'puntajePromedio': 800,
 					'comentarios': {
 						'comentario1': {
 							'comentarioId': 1,
-							'mensaje': 'Mensaje 1'
+							'mensaje': 'Mensaje 1',
+                            'puntajeAsignado': 100
 						},
 						'comentario2': {
 							'comentarioId': 2,
-							'mensaje': 'Mensaje 2'
+							'mensaje': 'Mensaje 2',
+                            'puntajeAsignado': 100
 						}
 					}
 				},
@@ -81,14 +205,17 @@ def comentarios():
 					'name': 'Agustin',
 					'lastname': 'Saturni',
 					'profesion': 'profesion',
+                    'puntajePromedio': 800,
 					'comentarios': {
 						'comentario1': {
 							'comentarioId': 1,
-							'mensaje': 'Mensaje 1'
+							'mensaje': 'Mensaje 1',
+                            'puntajeAsignado': 100
 						},
 						'comentario2': {
 							'comentarioId': 2,
-							'mensaje': 'Mensaje 2'
+							'mensaje': 'Mensaje 2',
+                            'puntajeAsignado': 100
 						}
 					}
 				},
@@ -96,14 +223,17 @@ def comentarios():
 					'name': 'Nicolas',
 					'lastname': 'Fuentes',
 					'profesion': 'profesion',
+                    'puntajePromedio': 800,
 					'comentarios': {
 						'comentario1': {
 							'comentarioId': 1,
-							'mensaje': 'Mensaje 1'
+							'mensaje': 'Mensaje 1',
+                            'puntajeAsignado': 100
 						},
 						'comentario2': {
 							'comentarioId': 2,
-							'mensaje': 'Mensaje 2'
+							'mensaje': 'Mensaje 2',
+                            'puntajeAsignado': 100
 						}
 					}
 				}
