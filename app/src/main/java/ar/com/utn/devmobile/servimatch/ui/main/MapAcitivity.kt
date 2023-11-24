b1e3af16b5272ff4d5a86b5e1fdbdceedc27d417
Hugo Peykovich <peykovn@gmail.com>
2023-11-24 18:13:05 -0300

    agrego pantalla basica de maps

--- app/src/main/java/ar/com/utn/devmobile/servimatch/ui/main/MapAcitivity.kt
+++ app/src/main/java/ar/com/utn/devmobile/servimatch/ui/main/MapAcitivity.kt
@@ -1,33 +1,43 @@
 package ar.com.utn.devmobile.servimatch.ui.main
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import ar.com.utn.devmobile.servimatch.ui.theme.Purpura1
 import ar.com.utn.devmobile.servimatch.ui.theme.Turquesa1
+import ar.com.utn.devmobile.servimatch.ui.theme.Turquesa5
 import com.google.maps.android.compose.GoogleMap
+import kotlin.math.round
 
 @Composable
 fun MapScreen(navController: NavController) {
@@ -36,36 +46,25 @@ fun MapScreen(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Turquesa1)
     ) {
-        Column {
-
-            TopBar(navController = navController, direccion = direccion, onChange = { value -> direccion = value })
-
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .background(Turquesa1)
-                    .fillMaxSize()
-                    .fillMaxWidth()
-                    .padding(horizontal = paddingH, vertical = paddingV)
-            ){
-
-                GoogleMap {
-
-                }
-            }
-        }
+        TopBar(navController = navController, direccion = direccion, onChange = { value -> direccion = value })
+        MyGoogleMap()
     }
 
 }
 
 @Composable
 fun TopBar(navController: NavController, direccion: String, onChange: (String) -> Unit) {
+    var isClearVisible by remember { mutableStateOf(false) }
+
+    DisposableEffect(direccion) {
+        isClearVisible = direccion.isNotEmpty()
+        onDispose { }
+    }
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Turquesa1)
             .zIndex(10f),
         verticalAlignment = Alignment.CenterVertically
 
@@ -82,11 +81,37 @@ fun TopBar(navController: NavController, direccion: String, onChange: (String) -
             value = direccion,
             onValueChange = { onChange(it) },
             modifier = Modifier
-                .padding(10.dp)
-                .background(Turquesa1),
-            leadingIcon = { Icon(imageVector = Icons.Default.LocationOn, contentDescription = "") }
+                .fillMaxWidth()
+                .padding(12.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(Color.White)
+                .alpha(1f)
+                .border(1.dp, Color.Black, shape = RoundedCornerShape(8.dp)),
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.LocationOn,
+                    contentDescription = "")
+            },
+            trailingIcon = {
+                if (isClearVisible) {
+                    IconButton(onClick = {
+                        onChange("")
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Clear,
+                            contentDescription = "Clear"
+                        )
+                    }
+                }
+            }
         )
     }
+}
+
+@Composable
+fun MyGoogleMap() {
+    GoogleMap (
+        modifier = Modifier.fillMaxSize().background(Turquesa5)
+    )
 }
 
 
