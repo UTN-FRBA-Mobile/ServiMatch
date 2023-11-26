ac50d88579a33bb2f09e4a57c1fc40f468b9e5db
Hugo Peykovich <peykovn@gmail.com>
2023-11-25 22:04:29 -0300

    agrego direcciones q funcionen

--- Api/usuarios.py
+++ Api/usuarios.py
@@ -3,12 +3,16 @@ USERS = [
         "username": "admin",
         "password": "admin",
         "token": " ",
-        "direccion": "Charcas 4148, Buenos Aires"
+        "direccion": "Av. Avellaneda 1946",
+        "latitud": -34.62220831534432,
+        "longitud": -58.458593672841175
     }, 
     {
         "username": "user10",
         "password": "123",
         "token": " ",
-        "direccion": "Av. Juan Bautista Alberdi 2428, C1406 CABA"
+        "direccion": "Juan Agustín García 2770",
+        "latitud": -34.60943,
+        "longitud": -58.48058
     }
 ]
